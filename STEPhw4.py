07ce92bef5ab39606362dd46b15ecbefe88278b3
msaitoo <m.saito017@gmail.com>
2016-06-21 15:13:55 +0900

    can process small and medium data

--- STEPhw4.py
+++ STEPhw4.py
@@ -1,20 +1,21 @@
 import sys
 
-if (len(sys.argv) != 2):
-    print "usage: python {} filename".format(sys.argv[0])
+if (len(sys.argv) != 3):
+    print "usage: python {} Filename NumberOfIteration".format(sys.argv[0])
     sys.exit()
 data = sys.argv[1]
+iteration = sys.argv[2]
 sampleinput = open (data, 'r')
 sample = sampleinput.readlines()
 
 def initVertex(sample):
-    "Create vertices with its name, points, number of links, and incoming points."
+    "Create vertices with its name, points, and number of links."
     numberofVertex = int(sample[0].rstrip("\n"))
     vertex = []
     links = []
     
     for i in range(1, numberofVertex+1):
-        vertex.append({'Vertex':sample[i].rstrip("\n"), 'point': 100.0, 'link': 0, 'in': 0})
+        vertex.append({'Vertex':sample[i].rstrip("\n"), 'point': 100, 'link': 0})
         links.append([])
     
     for i in range(int(numberofVertex)+2, len(sample)):
@@ -27,27 +28,32 @@ def initVertex(sample):
 
 def splitPoints(vertex, links):
     "Distribute points to its linked vertices."
+    distributed = []
+    
     for i in range(len(vertex)):
+        distributed.append(0)
         vertex[i]['point'] = vertex[i]['point'] / vertex[i]['link']
     
     for i in range(len(links)):
         for x in range(len(vertex)):
             for y in range(len(links[i])):
                 if links[i][y] == vertex[x]['Vertex']:
-                    vertex[x]['in'] += vertex[i]['point']
+                    distributed[x] += vertex[i]['point']
     
     for i in range(len(vertex)):
-        vertex[i]['point'] = vertex[i]['in']
-        vertex[i]['in'] = 0
+        vertex[i]['point'] = distributed[i]
+        distributed[i] = 0
     
     return (vertex, links)
 
-def repeatSplit (vertex, links):
+def repeatSplit (vertex, links, iteration):
     "Repeats splitting points."
-    for i in range(20):
+    for i in range(int(iteration)):
         points = splitPoints(vertex, links)
         vertex = points[0]
     return vertex
 
 vertex = initVertex(sample)
-result = repeatSplit(vertex[0], vertex[1])
+result = repeatSplit(vertex[0], vertex[1], iteration)
+
+print result
